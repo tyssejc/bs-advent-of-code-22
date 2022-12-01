6e409bc6df299929c1a0309fe02ad2c458964254
Charlie Tysse <charlie.tysse@searchdiscovery.com>
2022-12-01 17:17:03 -0500

    day 1 part 2 solution

--- 01/index.js
+++ 01/index.js
@@ -2,17 +2,28 @@ import { open } from 'node:fs/promises';
 
 try {
   const file = await open("./input.txt");
+
+
+  // pipe/compose would be much more elegant
   const raw = await file.readFile({ encoding: 'utf8'});
   
   const parsed = raw.split('\n\n').map((l) => l.split('\n'));
 
   const combined = parsed.map((g) => g.reduce((acc, val) => parseInt(val) + acc, 0));
 
-  console.log(combined);
-
   const sorted = combined.sort((a, b) => b - a);
 
-  console.log(sorted);
+  // part 1 solution
+  console.log(sorted[0]);
+
+  const topThree = sorted.reduce((acc, val, index) => {
+    if (index > 2) val = 0;
+    return acc + val;
+  }, 0);
+
+  // part 2 solution
+  console.log(topThree);
+
 
 } catch (e) {
   console.error(`um no we couldn't do that: ${e.message}`);
